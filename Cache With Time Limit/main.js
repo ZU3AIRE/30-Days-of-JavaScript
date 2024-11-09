ad4957c4f3e15fc9226c5a3e4a7ce387e312badc
Zubair Jamil <xubairjamil@gmail.com>
2024-11-09 21:01:47 +0500

    fix(leetcode): [#2622] Add The Result Logging

--- Cache With Time Limit/main.js
+++ Cache With Time Limit/main.js
@@ -63,6 +63,6 @@ TimeLimitedCache.prototype.count = function() {
 
 
 const timeLimitedCache = new TimeLimitedCache()
-timeLimitedCache.set(1, 42, 1000); // false
-timeLimitedCache.get(1) // 42
-timeLimitedCache.count() // 1
+console.log(timeLimitedCache.set(1, 42, 1000)); // false
+console.log(timeLimitedCache.get(1)) // 42
+console.log(timeLimitedCache.count()) // 1
